863878622f9d6b8fc8e107b5c1904a2e808facf9
nizamu1010 <nizamudheenjamali@gmail.com>
2023-05-03 13:44:59 +0530

    added

---
+++ ecommerce/ecommerceproject/shop/admin.py
@@ -0,0 +1,26 @@
+
+from django.contrib import admin
+from . models import Category, Product
+
+# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category,CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','price','stock','available','created','updated']
+    list_editable = ['price','stock','available']
+    prepopulated_fields = {'slug':('name',)}
+    list_per_page = 20
+
+admin.site.register(Product,ProductAdmin)
+
+
+'''  
+Username (leave blank to use 'dell'): shop
+Email address: shop@gmail.com
+pass : 1234
+'''
